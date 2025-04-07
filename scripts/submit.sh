d1e791419df3dbe7b8936b926de66b90c2a6348c
Mikael Lund <mlund@mac.com>
2025-04-07 11:30:17 +0200

    Add project id to submit script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,6 +3,9 @@
 # Use this submit script directly in the faunus run folder.
 # Adjust time as needed
 
+# Select project (only needed if user has access to multiple projects)
+#SBATCH -A lu2025-2-42
+
 # Number of nodes. We run Faunus in serial, so this should always be 1.
 #SBATCH -N 1
 
@@ -10,7 +13,7 @@
 #SBATCH --ntasks-per-node=1
 
 # Maximim allocated time hh::mm::ss
-#SBATCH -t 00:10:00
+#SBATCH -t 50:00:00
 
 # You must have created the environment before submitting
 module add Miniforge3
